812ade35fdf5920019822eb4002b4801c7326e72
CarsonJM596 <124809808+CarsonJM596@users.noreply.github.com>
2026-04-14 11:37:19 -0500

    update added

--- routes/costumes.js
+++ routes/costumes.js
@@ -6,4 +6,5 @@ var router = express.Router();
 router.get('/', costume_controllers.costume_view_all_Page);
 router.get('/detail', costume_controllers.costume_view_one_Page);
 router.get('/create', costume_controllers.costume_create_Page);
+router.get('/update', costume_controllers.costume_update_Page);
 module.exports = router;
