ebf2f5c7a25b0526ba6b3b2fd9ad7bbcb70bb1e8
CarsonJM596 <124809808+CarsonJM596@users.noreply.github.com>
2026-04-21 20:32:59 -0500

    Part 1 and 2

--- routes/costumes.js
+++ routes/costumes.js
@@ -2,10 +2,23 @@ var express = require('express');
 const costume_controllers = require('../controllers/costume');
 var router = express.Router();
 
+// 🔐 Middleware to check if user is logged in
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
 // GET costumes page
 router.get('/', costume_controllers.costume_view_all_Page);
+
+// Public routes (still accessible without login)
 router.get('/detail', costume_controllers.costume_view_one_Page);
-router.get('/create', costume_controllers.costume_create_Page);
-router.get('/update', costume_controllers.costume_update_Page);
-router.get('/delete', costume_controllers.costume_delete_Page);
+
+// 🔒 Protected routes
+router.get('/create', secured, costume_controllers.costume_create_Page);
+router.get('/update', secured, costume_controllers.costume_update_Page);
+router.get('/delete', secured, costume_controllers.costume_delete_Page);
+
 module.exports = router;
